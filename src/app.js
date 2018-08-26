b9f1a6120d935b1f381e6f72a6c5addc38dd3969
hhp <624791164@qq.com>
2018-08-26 10:13:42 +0800

    ok

--- src/app.js
+++ src/app.js
@@ -13,18 +13,43 @@ class App extends Component {
       navigationBarBackgroundColor: '#fff',
       navigationBarTitleText: 'WeChat',
       navigationBarTextStyle: 'black'
+    },
+    tabBar: {
+      color: '#676767',
+      selectedColor: '#63BA74',
+      borderStyle: 'black',
+      list: [
+        {
+          pagePath: 'pages/index/index',
+          text: '首页',
+          iconPath: './asset/tabbar/home.png',
+          selectedIconPath: './asset/tabbar/homed.png'
+        },
+        {
+          pagePath: 'pages/index/index',
+          text: '购物车',
+          iconPath: './asset/tabbar/shop.png',
+          selectedIconPath: './asset/tabbar/shopd.png'
+        },
+        {
+          pagePath: 'pages/index/index',
+          text: '我的',
+          iconPath: './asset/tabbar/mine.png',
+          selectedIconPath: './asset/tabbar/mined.png'
+        }
+      ]
     }
   }
 
-  componentDidMount () {}
+  componentDidMount() { }
 
-  componentDidShow () {}
+  componentDidShow() { }
 
-  componentDidHide () {}
+  componentDidHide() { }
 
-  componentCatchError () {}
+  componentCatchError() { }
 
-  render () {
+  render() {
     return (
       <Index />
     )
