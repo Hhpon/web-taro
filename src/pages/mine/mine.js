df86149e4579a75c8df99c68827959b031cc7ece
hhp <624791164@qq.com>
2018-10-01 23:25:39 +0800

    新增 mine 界面

--- src/pages/mine/mine.js
+++ src/pages/mine/mine.js
@@ -1,5 +1,6 @@
 import Taro, { Component } from '@tarojs/taro'
-import { View } from '@tarojs/components'
+import { View, Image, Text } from '@tarojs/components'
+import { AtIcon } from 'taro-ui'
 
 import './mine.scss'
 
@@ -11,12 +12,53 @@ export default class mine extends Component {
     constructor() {
         super();
         this.state = {
+            userInfo: {}
         }
+    }
+
+    componentWillMount() {
+        const openId = Taro.getStorageSync('openid');
+        Taro.request({
+            url: 'http://localhost:7001/getUserinfo',
+            data: {
+                openId: openId
+            }
+        }).then(res => {
+            this.setState({
+                userInfo: res.data
+            })
+            console.log(res.data);
+        })
     }
 
     render() {
         return (
-            <View>123</View>
+            <View>
+                <View className='message-container'>
+                    <View className='image-container'>
+                        <View style='height:10px;'></View>
+                        <View className='avatar-con'>
+                            <View className='avatar'>
+                                <Image src={this.state.userInfo.avatarUrl} style='width:70px;height:70px;'></Image>
+                            </View>
+                        </View>
+                        <View className='name-con'>{this.state.userInfo.nickName}</View>
+                    </View>
+                    <View className='mine-order'>
+                        <View className='order-top'>
+                            <Text>我的订单</Text>
+                            <View className='ordertop-right'>
+                                <Text>查看全部订单 &nbsp;</Text>
+                                <AtIcon value='chevron-right' size='15' color='#868281' className='right-icon'></AtIcon>
+                            </View>
+                        </View>
+                        <View></View>
+                    </View>
+                    <View></View>
+                </View>
+                <View></View>
+                <View></View>
+            </View>
         )
     }
 }
